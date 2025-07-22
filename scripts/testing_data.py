e3ae89cf1dc40ed86b6d08d60e0dbafbd9eb7f03
Caleb Reagor <ccreagor@gmail.com>
2025-07-22 18:41:32 +0200

    MesNet analysis of 3CA neuroendocrine datasets

--- scripts/testing_data.py
+++ scripts/testing_data.py
@@ -1,43 +1,50 @@
 #%%
 import os, glob
-import numpy as np
 import pandas as pd
 import scanpy as sc
 import anndata as ad
+from scipy.sparse import csr_matrix
 
+# datasets summary
+summary_df = pd.read_csv(os.path.join('..', 'data', 'summary.csv'), index_col = 0)
+summary_df = summary_df.loc[summary_df.Split == 'Testing']
+species_dict = summary_df.Species.to_dict()
+
+# processed datasets
+adata_fn = sorted(glob.glob(os.path.join('..', 'data', 'processed', 'Testing', '*', '*.h5ad')))
+adata_dict = {os.path.split(fn)[1].replace('.h5ad', '') : fn for fn in adata_fn}
 adata_training = sc.read_h5ad(os.path.join('..', 'data', 'modeling', 'training.h5ad'))
-adata_testing = sc.read_h5ad(os.path.join('..', 'data', 'processed', 'CCCA_Neuroendocrine.h5ad'))
 
+# datasets features
 feat_fn = os.path.join('..', 'data', 'features', 'biomart', 'union.csv')
 feat_union = pd.read_csv(feat_fn)
 
-#%%
-# keep features
-features_msk = adata_testing.var_names.isin(feat_union['hsapiens'])
-adata_testing = adata_testing[~adata_testing.obs.celltype.isna(), features_msk]
+# prepare testing datasets
+for key in adata_dict:
+    species = species_dict[key]
+    adata = sc.read_h5ad(adata_dict[key])
 
-#%%
-# mouse genes (homologs)
-var_dict = feat_union.set_index('hsapiens').mmusculus.to_dict()
-adata_testing.var_names = adata_testing.var_names.map(var_dict)
+    # keep malignant cells, features
+    features_msk = adata.var_names.isin(feat_union[species])
+    adata = adata[adata.obs.celltype == 'Malignant', features_msk]
 
-#%%
-# Create new matrix with zeros, same shape as (n_obs, n_genes in reference)
-X_new = np.zeros((adata_testing.n_obs, adata_training.n_vars))
+    # mouse genes (homologs)
+    if species != 'mmusculus':
+        var_dict = feat_union.set_index(species).mmusculus.to_dict()
+        adata.var_names = adata.var_names.map(var_dict)
+    adata_dict[key] = adata
 
-# Fill in values for shared genes
-shared_genes = adata_training.var_names.intersection(adata_testing.var_names)
-X_new[:, [adata_training.var_names.get_loc(g) for g in shared_genes]] = adata_testing[:, shared_genes].X.toarray()
+# concatenate datasets
+adata = ad.concat(adata_dict, join = 'outer', merge = 'same', label = 'source')
+adata.obs['weight'] = 1.
 
-# Rebuild adata with aligned genes
-adata_testing = ad.AnnData(X = X_new, obs = adata_testing.obs.copy(), var = adata_training.var.copy())
-adata_testing.var_names = adata_training.var_names
+# training features
+adata_testing = ad.AnnData(X = csr_matrix((adata.shape[0], adata_training.shape[1]), dtype = adata.X.dtype))
+adata_testing.var = adata_training.var.copy()
+adata_testing.obs = adata.obs.copy()
+adata_testing[:, adata.var_names].X = adata.X
 
-#%%
-from scipy.sparse import csr_matrix
-adata_testing.X = csr_matrix(adata_testing.X)
-
-#%%
+# save dataset
 adata_testing.write(os.path.join('..', 'data', 'modeling', 'testing.h5ad'))
 
 #%%
