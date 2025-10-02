f323e19fd9aecd73ecee7b6af32ea7831bbc19e5
Caleb Reagor <ccreagor@gmail.com>
2025-10-02 14:59:53 +0200

    Embedding figures for dev, 3CA, and TCGA

--- preprocessing/scripts/TCGA.py
+++ preprocessing/scripts/TCGA.py
@@ -1,35 +1,35 @@
 #%%
 import os
+import numpy as np
 import pandas as pd
 import scanpy as sc
 import anndata as ad
 from scipy.sparse import csr_matrix
 
-name = 'LGG'
-pth = os.path.join('..', '..')
-pth_in = os.path.join(pth, 'data', 'unzip', f'TCGA-{name}')
-pth_feat = os.path.join(pth, 'data', 'features', 'biomart')
-pth_out = os.path.join(pth, 'data', 'modeling')
+pth = os.path.join('..', '..', 'data')
+pth_in = os.path.join(pth, 'unzip', 'TCGA')
+pth_feat = os.path.join(pth, 'features', 'biomart')
+pth_out = os.path.join(pth, 'modeling')
 
-# load dataset [log2(1 + avg_TPM) -> log1p(avg_TPM)]
-fn_data = os.path.join(pth_in, f'TCGA-{name}.star_tpm.tsv')
+# load dataset [log2(TPM + 1e-3) -> log1p(TPM)]
+fn_data = os.path.join(pth_in, 'tcga_RSEM_gene_tpm.tsv')
 adata = sc.read_csv(fn_data, delimiter = '\t').T
-adata.X = csr_matrix(2 ** adata.X - 1)
+adata.X = np.exp2(adata.X) - 1e-3
+adata.X = np.clip(adata.X, 0., None)
+adata.X = csr_matrix(adata.X)
 sc.pp.log1p(adata)
 
 # map IDs
-fn_map = os.path.join(pth_in, 'gencode.v36.annotation.gtf.gene.probemap.tsv')
+fn_map = os.path.join(pth_in, 'gencode.v23.annotation.gene.probemap.tsv')
 id_dict = pd.read_csv(fn_map, delimiter = '\t', index_col = 0).gene.to_dict()
 adata.var_names = adata.var_names.map(id_dict)
 
-# load metadata (clinical, survival)
-fn_clinical = os.path.join(pth_in, f'TCGA-{name}.clinical.tsv')
-fn_survival = os.path.join(pth_in, f'TCGA-{name}.survival.tsv')
-df_clinical = pd.read_csv(fn_clinical, delimiter = '\t', index_col = 0)
-df_survival = pd.read_csv(fn_survival, delimiter = '\t', index_col = 0)
-df_obs = pd.concat((df_clinical, df_survival), axis = 1)
-df_obs['source'] = 'CCCA'
-adata.obs = adata.obs.join(df_obs.astype(str), how = 'left')
+# load phenotype data
+fn_pheno = os.path.join(pth_in, 'Survival_SupplementalTable_S1_20171025_xena_sp.tsv')
+df = pd.read_csv(fn_pheno, delimiter = '\t', index_col = 0)
+df['source'] = 'TCGA'   # cat code = 0 (same as CCCA)
+adata.obs = adata.obs.join(df.astype(str), how = 'left')
+adata = adata[~adata.obs.source.isna()].copy()
 
 # keep features (mouse genes [homologs])
 features = pd.read_csv(os.path.join(pth_feat, 'union.csv'))
@@ -43,6 +43,6 @@ adata_dev = sc.read_h5ad(os.path.join(pth_out, 'development.h5ad'))
 X = csr_matrix((adata.shape[0], adata_dev.shape[1]), dtype = adata.X.dtype)
 X[:, adata_dev.var_names.get_indexer(adata.var_names)] = adata.X.copy()
 adata_out = ad.AnnData(X = X, var = adata_dev.var, obs = adata.obs)
-adata_out.write(os.path.join(pth_out, f'TCGA_{name}.h5ad'))
+adata_out.write(os.path.join(pth_out, 'TCGA.h5ad'))
 
 #%%
