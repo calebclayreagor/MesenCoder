f0b2ce021f722e3b6ce0cca1efc74f1600928804
Caleb Reagor <ccreagor@gmail.com>
2025-12-02 18:06:25 +0100

    Removed duplicate genes from trajectory modules

--- preprocessing/scripts/homologs.py
+++ preprocessing/scripts/homologs.py
@@ -5,16 +5,17 @@ import pandas as pd
 from pybiomart import Server
 server = Server(host = 'http://www.ensembl.org')
 mart = server.marts['ENSEMBL_MART_ENSEMBL']
-pth = os.path.join('..', '..')
 
-outdir = os.path.join(pth, 'data', 'features', 'biomart')
+pth = os.path.join('..', '..')
+pth_data = os.path.join(pth, 'data')
+outdir = os.path.join(pth_data, 'features', 'biomart')
 
 # datasets summary
-summary_df = pd.read_csv(os.path.join(pth, 'data', 'summary.csv'), index_col = 0)
+summary_df = pd.read_csv(os.path.join(pth_data, 'summary.csv'), index_col = 0)
 species_dict = summary_df.Species.to_dict()
 
 # dataset features
-feat_fn = sorted(glob.glob(os.path.join(pth, 'data', 'features', '*.txt')))
+feat_fn = sorted(glob.glob(os.path.join(pth_data, 'features', '*.txt')))
 feat_dict = {os.path.split(fn)[1].replace('.txt', '') : fn for fn in feat_fn}
 
 mart_dict = {
@@ -41,6 +42,10 @@ for i, key in enumerate(feat_dict):
     species = species_dict[key]
     df = mart_dict[species].copy()
     df = df.loc[df[species].isin(features)].reset_index(drop = True)
+    df = df.sort_values(['mmusculus', 'hsapiens']).reset_index(drop = True)
+    df = df.drop_duplicates(subset = ['mmusculus'], keep = 'first')
+    df = df.drop_duplicates(subset = ['hsapiens'], keep = 'first')
+    df = df.sort_values('mmusculus').reset_index(drop = True)
     df.to_csv(os.path.join(outdir, f'{key}.csv'), index = False)
     feat_dict[key] = df
     # print(key, df)
@@ -50,6 +55,6 @@ df = df.sort_values(['mmusculus', 'hsapiens']).reset_index(drop = True)
 df = df.drop_duplicates(subset = ['mmusculus'], keep = 'first')
 df = df.drop_duplicates(subset = ['hsapiens'], keep = 'first')
 df = df.sort_values('mmusculus').reset_index(drop = True)
-df.to_csv(os.path.join(outdir, 'union.csv'), index=False)
+df.to_csv(os.path.join(outdir, 'union.csv'), index = False)
 
 #%%
