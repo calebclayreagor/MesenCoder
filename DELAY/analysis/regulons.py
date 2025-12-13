7d38f0ba62783b0f65dbe5eb8357eb6f209908c0
Caleb Reagor <ccreagor@gmail.com>
2025-12-13 22:04:07 +0100

    Remove Foxc1, Prrx2 plots

--- DELAY/analysis/regulons.py
+++ DELAY/analysis/regulons.py
@@ -6,9 +6,7 @@ pth = os.path.join('..', '..', 'data')
 pth_in = os.path.join(pth, 'DELAY')
 pth_feat = os.path.join(pth, 'features', 'biomart')
 pth_out = os.path.join(pth_in, 'regulons')
-
 g = pd.read_csv(os.path.join(pth_feat, 'union.csv'))
-reg = ('Gata4', 'Twist1', 'Snai1', 'Foxc1', 'Prrx2')
 
 # load GRN
 fn = os.path.join(pth_in, 'networkTop20.csv')
@@ -18,7 +16,7 @@ A.columns = A.columns.str.capitalize()
 A = A.rename(columns = {'1700017b05rik' : '1700017B05Rik'})
 
 # save regulons
-for name in reg:
+for name in ('Gata4', 'Twist1', 'Snai1'):
     tgt = A.columns[A.loc[name]]
     g_out = g.loc[g.mmusculus.isin(tgt)]
     fn = os.path.join(pth_out, f'{name}.csv')
