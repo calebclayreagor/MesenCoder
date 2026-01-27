b0c0183da2089a282012b339dc927e2cb5a56a35
Caleb Reagor <ccreagor@gmail.com>
2026-01-27 04:47:51 +0100

    Updated CCCA analysis and figures

--- TCGA/preprocessing.py
+++ TCGA/preprocessing.py
@@ -3,13 +3,11 @@ import os
 import numpy as np
 import pandas as pd
 import scanpy as sc
-import anndata as ad
 from scipy.sparse import csr_matrix
 
-pth = os.path.join('..', '..', 'data')
+pth = os.path.join('..', 'data')
 pth_in = os.path.join(pth, 'unzip', 'TCGA')
-pth_feat = os.path.join(pth, 'features', 'biomart')
-pth_out = os.path.join(pth, 'modeling', 'inputs')
+pth_out = os.path.join(pth, 'TCGA')
 
 # load dataset [log2(TPM + 1e-3) -> log1p(TPM)]
 fn_data = os.path.join(pth_in, 'tcga_RSEM_gene_tpm.tsv')
@@ -23,23 +21,14 @@ sc.pp.log1p(adata)
 fn_map = os.path.join(pth_in, 'gencode.v23.annotation.gene.probemap.tsv')
 id_dict = pd.read_csv(fn_map, delimiter = '\t', index_col = 0).gene.to_dict()
 adata.var_names = adata.var_names.map(id_dict)
+adata.var_names_make_unique()
 
 # load phenotype data
 fn_pheno = os.path.join(pth_in, 'Survival_SupplementalTable_S1_20171025_xena_sp.tsv')
 df = pd.read_csv(fn_pheno, delimiter = '\t', index_col = 0)
 adata.obs = adata.obs.join(df.astype(str), how = 'left')
 
-# keep features (mouse genes [homologs])
-features = pd.read_csv(os.path.join(pth_feat, 'union.csv'))
-adata = adata[:, adata.var_names.isin(features.hsapiens)]
-var_dict = features.set_index('hsapiens').mmusculus.to_dict()
-adata.var_names = adata.var_names.map(var_dict)
-
-# all dev features
-adata_dev = sc.read_h5ad(os.path.join(pth_out, 'development.h5ad'))
-X = csr_matrix((adata.shape[0], adata_dev.shape[1]), dtype = adata.X.dtype)
-X[:, adata_dev.var_names.get_indexer(adata.var_names)] = adata.X.copy()
-adata_out = ad.AnnData(X = X, var = adata_dev.var, obs = adata.obs)
-adata_out.write(os.path.join(pth_out, 'TCGA.h5ad'))
+# save output
+adata.write(os.path.join(pth_out, 'PANCAN.h5ad'))
 
 #%%
